2d88b8860089f4ded0976d58ea2231c093373db7
pw <leboudin@163.com>
2026-05-11 00:17:27 +0800

    updated next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -10,6 +10,14 @@ const nextConfig: NextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 512, 768, 1024, 1280],
     minimumCacheTTL: 60,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
   env: {
     /**
